963ea4c438383fcc645d946c35670e8310904845
Thanh Tan <dp1.1a1.tan@gmail.com>
2024-05-18 14:18:07 +0700

    continue making about information of student

--- management application interface/Management Students System/Management application/Properties/Resources.Designer.cs
+++ management application interface/Management Students System/Management application/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Management_application.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _158252126948421 {
+            get {
+                object obj = ResourceManager.GetObject("158252126948421", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap _3094352 {
             get {
                 object obj = ResourceManager.GetObject("3094352", resourceCulture);
